e6ef4757ecfe39691247f734a6b35e9e9835a276
Timothee Guignard <guignard.timothee@gmail.com>
2026-09-09 17:27:51 +0200

    minor fixes

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name ?? assetInfo.names?.[0] ?? '';
+          return name.endsWith('.css')
+            ? '[name]-[hash][extname]'
+            : 'assets/[name]-[hash][extname]';
+        },
+      },
+    },
+  },
 });
